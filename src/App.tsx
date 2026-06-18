208eca53920537d7b7a05ee9d54432d3853acf5d
themetriclab <themetriclab@gmail.com>
2026-06-18 12:47:37 -0400

    Add files via upload

--- src/App.tsx
+++ src/App.tsx
@@ -2,13 +2,11 @@ import { Toaster } from "@/components/ui/toaster";
 import { Toaster as Sonner } from "@/components/ui/sonner";
 import { TooltipProvider } from "@/components/ui/tooltip";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
-import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import Index from "./pages/Index";
 import TourDetail from "./pages/TourDetail";
 import Contact from "./pages/Contact";
 import NotFound from "./pages/NotFound";
-import PolarBearChurchillLanding from "./pages/PolarBearChurchillLanding";
-import MooseOntarioAlgonquinLanding from "./pages/MooseOntarioAlgonquinLanding";
 import Blog from "./pages/Blog";
 import BlogPost from "./pages/BlogPost";
 import ScrollToTop from "./components/ScrollToTop";
@@ -28,8 +26,8 @@ const App = () => (
           <Route path="/contact" element={<Contact />} />
           <Route path="/blog" element={<Blog />} />
           <Route path="/blog/:slug" element={<BlogPost />} />
-          <Route path="/polar-bear-photography-tours-churchill-manitoba" element={<PolarBearChurchillLanding />} />
-          <Route path="/moose-photography-tours-ontario-algonquin-park" element={<MooseOntarioAlgonquinLanding />} />
+          <Route path="/polar-bear-photography-tours-churchill-manitoba" element={<Navigate to="/tours/polar-bear-photography-tour" replace />} />
+          <Route path="/moose-photography-tours-ontario-algonquin-park" element={<Navigate to="/tours/moose-photography-tours" replace />} />
           {/* ADD ALL CUSTOM ROUTES ABOVE THE CATCH-ALL "*" ROUTE */}
           <Route path="*" element={<NotFound />} />
         </Routes>
